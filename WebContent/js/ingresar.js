c3da4e2156ef5ca1329bad683b7051e3d4de8974
Esteban Beiza <esteban.beiza@gmail.com>
2020-06-22 12:44:13 -0400

    css y js

---
+++ WebContent/js/ingresar.js
@@ -0,0 +1,27 @@
+$('#btn-sesion').click(function(){
+    window.open("ingresar.html", '_self');
+    return false;
+});
+
+$('#btn-ingresar').click(function(){
+	alert("hola");
+    window.open("misprestamos.html", '_self');
+    return false;
+});
+
+$('#btn-agregar').click(function(){
+    window.open("misprestamos.html", '_self');
+    return false;
+});
+
+/*
+$('#btn-ingresar').click(function(){
+    window.open("misprestamos.html", '_self');
+});
+
+$('#btn-agregar').click(function(){
+    window.open("misprestamos.html", '_self');
+});
+
+
+*/
